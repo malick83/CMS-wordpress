07aabda1eab0e7e300ee7f2549224f5dd66125b8
malick83 <45153624+malick83@users.noreply.github.com>
2022-01-18 12:10:41 +0000

    Update wp-config.php
    ajout de quelques configurations

--- wordpress1/wp-config.php
+++ wordpress1/wp-config.php
@@ -28,7 +28,7 @@ define( 'DB_NAME', 'teste' );
 define( 'DB_USER', 'root' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'Ballagaye7@' );
+define( 'DB_PASSWORD', '' );
 
 /** Adresse de l’hébergement MySQL. */
 define( 'DB_HOST', 'localhost' );
